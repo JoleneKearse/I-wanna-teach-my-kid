3ceb1bd05e1d411cfc0b2af74a823f97dba76493
JoleneKearse <pharaohnutz@gmail.com>
2024-09-02 17:11:13 -0400

    chore: remove boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h2>Topics to cover</h2>
+      <ul>
+        <li>Numbers</li>
+        <li>Colors</li>
+        <li>Shapes</li>
+        <li>Animals</li>
+        <li>Family</li>
+        <li>Action words</li>
+        <li>Food</li>
+        <li>Places</li>
+        <li>Weather</li>
+        <li>Time</li>
+        <li>Fruit</li>
+        <li>Clothes</li>
+        <li>Transport</li>
+        <li>Jobs</li>
+        <li>Vegetables</li>
+        <li>Feelings</li>
+        <li>Body parts</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
